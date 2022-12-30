a4bfd9c4289e17d5611f251ccd50fb62ec123eef
devforkronos <109998051+devforkronos@users.noreply.github.com>
2022-12-29 22:02:46 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
     // Discord Token
     token: "your discord bot token",
-    command: "?createautorole",
+    command: "$verify",
     // Btw you can cutom the commands like !create , $create , etc.
 
     // Embed Settings
     embed: {
         color: "#5865F2", //https://htmlcolorcodes.com/color-picker/
-        footer: "source code of nexttectzx bot - dev : jn03" //Btw you can change but pls give me an credit :D
+        footer: "pluto = god" //Btw you can change but pls give me an credit :D
     },
 
     // Perms Can change the perms if wish
